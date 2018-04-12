b654ccab40859fd5866416acdcc3c50371c87154
nbesagni <36893225+nbesagni@users.noreply.github.com>
2018-04-12 23:43:56 +0100

    Add project files.

---
+++ VoteWithYourWallet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VoteWithYourWallet.Startup))]
+namespace VoteWithYourWallet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
